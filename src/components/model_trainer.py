f6a94ecb4f43028791d6c594eab71097561c95c2
Aishwarya Vadlamudi <aish.vadlamudi@gmail.com>
2026-04-09 22:21:44 -0700

    added hyperparameter tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,6 +21,7 @@ from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object, evaluate_models
 
+from src.components.data_ingestion import DataIngestion
 from src.components.data_transformation import DataTransformation
 
 
@@ -33,24 +34,88 @@ class ModelTrainer:
     def __init__(self):
         self.model_trainer_config = ModelTrainerConfig()
 
-    def initiate_model_trainer(self, train_array, test_array, preprocessor_path):
+    def initiate_model_trainer(self, train_array, test_array):
         try:
             logging.info("Splitting training and test input data")
             X_train, y_train = train_array[:, :-1], train_array[:, -1]
             X_test, y_test = test_array[:, :-1], test_array[:, -1]
 
             models = {
-                "Decision Tree": DecisionTreeRegressor(),
-                "Random Forest": RandomForestRegressor(),
-                "AdaBoost": AdaBoostRegressor(),
-                "Gradient Boosting": GradientBoostingRegressor(),
-                "KNN": KNeighborsRegressor(),
-                "Linear Regression": LinearRegression(),
-                "Lasso": Lasso(),
-                "Ridge": Ridge(),
-                "SVR": SVR(),
-                "CatBoost": CatBoostRegressor(verbose=False),
-                "XGBoost": XGBRegressor(),
+                "Decision Tree": {
+                    "model": DecisionTreeRegressor(),
+                    "params": {
+                        "criterion": [
+                            "squared_error",
+                            "friedman_mse",
+                            "absolute_error",
+                            "poisson",
+                        ],
+                        # 'splitter':['best','random'],
+                        # 'max_features':['sqrt','log2'],
+                    },
+                },
+                "Random Forest": {
+                    "model": RandomForestRegressor(),
+                    "params": {
+                        # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                        # 'max_features':['sqrt','log2',None],
+                        "n_estimators": [8, 16, 32, 64, 128, 256]
+                    },
+                },
+                "AdaBoost": {
+                    "model": AdaBoostRegressor(),
+                    "params": {
+                        "learning_rate": [0.1, 0.01, 0.5, 0.001],
+                        # 'loss':['linear','square','exponential'],
+                        "n_estimators": [8, 16, 32, 64, 128, 256],
+                    },
+                },
+                "Gradient Boosting": {
+                    "model": GradientBoostingRegressor(),
+                    "params": {
+                        # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                        "subsample": [0.6, 0.7, 0.75, 0.8, 0.85, 0.9],
+                        # 'criterion':['squared_error', 'friedman_mse'],
+                        # 'max_features':['auto','sqrt','log2'],
+                        "n_estimators": [8, 16, 32, 64, 128, 256],
+                    },
+                },
+                "KNN": {
+                    "model": KNeighborsRegressor(),
+                    "params": {},
+                },
+                "Linear Regression": {
+                    "model": LinearRegression(),
+                    "params": {},
+                },
+                "Lasso": {
+                    "model": Lasso(),
+                    "params": {},
+                },
+                "Ridge": {
+                    "model": Ridge(),
+                    "params": {},
+                },
+                "SVR": {
+                    "model": SVR(),
+                    "params": {},
+                },
+                "CatBoost": {
+                    "model": CatBoostRegressor(verbose=False),
+                    "params": {
+                        "depth": [6, 8, 10],
+                        "learning_rate": [0.01, 0.05, 0.1],
+                        "iterations": [30, 50, 100],
+                    },
+                },
+                "XGBoost": {
+                    "model": XGBRegressor(),
+                    "params": {
+                        "learning_rate": [0.1, 0.01, 0.05, 0.001],
+                        "n_estimators": [8, 16, 32, 64, 128, 256],
+                    },
+                },
             }
 
             model_report: dict = evaluate_models(
@@ -82,8 +147,13 @@ class ModelTrainer:
 
 
 if __name__ == "__main__":
+    di_obj = DataIngestion()
+    train_data_path, test_data_path = di_obj.initiate_data_ingestion()
+
     dt_obj = DataTransformation()
-    train_array, test_array, preprocessor_path = dt_obj.initiate_data_transformation()
+    train_array, test_array, preprocessor_path = dt_obj.initiate_data_transformation(
+        train_data_path, test_data_path
+    )
 
     model_obj = ModelTrainer()
-    model_obj.initiate_model_trainer(train_array, test_array, preprocessor_path)
+    model_obj.initiate_model_trainer(train_array, test_array)
--- src/utils.py
+++ src/utils.py
@@ -10,6 +10,7 @@ import sys
 import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.logger import logging
 from src.exception import CustomException
@@ -47,10 +48,12 @@ def evaluate_models(X_train, y_train, X_test, y_test, models):
     try:
         logging.info("Evaluating models")
         model_report = {}
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_train_pred = model.predict(X_train)
-            y_test_pred = model.predict(X_test)
+        for model_name, model_info in models.items():
+
+            gs = GridSearchCV(model_info["model"], model_info["params"])
+            gs.fit(X_train, y_train)
+            y_train_pred = gs.predict(X_train)
+            y_test_pred = gs.predict(X_test)
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
             model_report[model_name] = test_model_score
